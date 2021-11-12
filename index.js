eb54ab850fec03302d27f6e5296120913eca8bf4
Andrew <andrew@Andrews-MacBook-Pro.local>
2021-11-12 10:37:43 +0300

    update

---
+++ index.js
@@ -0,0 +1,11 @@
+/*const http = require('http');
+const port = 3000;
+const hostname = '127.0.0.1';
+
+const server = http.createServer();
+
+*/
+
+const addThree = num => num +3;
+
+console.log(addThree(4));
